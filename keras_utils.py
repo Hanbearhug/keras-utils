693ec9ad34fc310d09f6d7ca91330b3931773281
Hanbearhug <38373160+Hanbearhug@users.noreply.github.com>
2019-04-06 13:18:43 +0800

    Update keras_utils.py

--- keras_utils.py
+++ keras_utils.py
@@ -163,3 +163,25 @@ def f1(y_true, y_pred):
     precision = precision(y_true, y_pred)
     recall = recall(y_true, y_pred)
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+
+"""
+读取预训练的词向量，如果预训练词向量中没有需要的词向量，按照均值和方差相同的正态分布初始化
+"""
+
+def load_glove(word_index):
+    EMBEDDING_FILE = '../input/embeddings/glove.840B.300d/glove.840B.300d.txt'
+    def get_coefs(word,*arr): return word, np.asarray(arr, dtype='float32')
+    embeddings_index = dict(get_coefs(*o.split(" ")) for o in open(EMBEDDING_FILE) if o.split(" ")[0] in word_index)
+
+    all_embs = np.stack(embeddings_index.values())
+    emb_mean,emb_std = all_embs.mean(), all_embs.std()
+    embed_size = all_embs.shape[1]
+
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (max_features, embed_size))
+    for word, i in word_index.items():
+        if i >= max_features: continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None: embedding_matrix[i] = embedding_vector
+            
+    return embedding_matrix 
